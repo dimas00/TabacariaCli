1cc185282e44c267727f8259509f97066d858599
202010442 <dimasfernandes00@gmail.com>
2022-10-25 11:26:12 -0300

    segundo commit

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'tb-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+
+    
+}
---
+++ src/app/produtos/produto.module.ts
@@ -0,0 +1,8 @@
+import { NgClass } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: []
+})
+
+export class ProdutoModule{}
